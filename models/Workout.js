71fb8215a623d041197a109123ae303ef292a5f9
Haydenrust1 <haydenrust1@gmail.com>
2020-10-31 14:20:34 -0700

    defined field types for exercises

---
+++ models/Workout.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date
+    },
+    exercises: {
+        type: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+    }
+});
+
+const Workout = mongoose.model("Workouts", WorkoutSchema);
+
+module.exports = Workout;
